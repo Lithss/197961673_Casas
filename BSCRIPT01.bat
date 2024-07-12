b8b6b76ed4937ba8183caec65bd2135815f9944c
Lithss <yupyep1234567@gmail.com>
2024-07-12 16:29:50 +0800

    BSCRIPT01 batch file have basic commands

---
+++ BSCRIPT01.bat
@@ -0,0 +1,8 @@
+start https://www.youtube.com/
+start https://x.com/home
+start https://www.facebook.com/
+start https://www.binance.com/
+start https://uc-bcf.instructure.com/
+start C:\Windows\system32\calc.exe
+start C:\Windows\system32\notepad.exe
+start shutdown /s /t 10
